316504dc33e9f4debd9571fe88593c8e657a892c
luckysitara <udoessienprecious@gmail.com>
2024-03-12 21:57:45 -0700

    updating puppet files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,11 @@
-#Change ULIMIT value from 15 to 2000
-exec {'/etc/default/nginx':
-command  => 'sed -i "s/15/500/g" /etc/default/nginx; sudo service nginx restart',
-provider => shell,
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/2000/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,11 @@
-#Increases limit of open files
-exec {'hard-limit':
-    command  => 'sed -i "/holberton hard/s/5/10000" /etc/security/limits.conf',
-    provider => shell,
+# This manuscript enables the user holberton to login and open files without error
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-exec {'soft-limit':
-    command  => 'sed -i "/holberton hard/s/4/10000" /etc/security/limits.conf',
-    provider => shell,
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
